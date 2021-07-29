036e0e94370505baff134a41ad4f54e0503853a8
Bender Sarmiento <ismaelantonio.sarmiento@gmail.com>
2021-07-29 10:36:13 +0200

    * Enrichment TryExtensions.scala * Enrichment TryExtensionsTest.scala
    Signed-off-by: Bender Sarmiento <ismaelantonio.sarmiento@gmail.com>

--- src/main/scala/futures/TryExtensions.scala
+++ src/main/scala/futures/TryExtensions.scala
@@ -1,5 +1,8 @@
 package futures
 
+import futures.TryExtensions.{CLASS_CAST_EXCEPTION_MESSAGE, TRASH}
+
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.language.implicitConversions
 import scala.util.{Failure, Success, Try}
@@ -7,33 +10,88 @@ import scala.util.{Failure, Success, Try}
 
 object TryExtensions {
 
+  implicit def errorHandlingIterable[T](src: Iterable[Try[T]]): ErrorsHandling[T] = ErrorsHandling(src)
+
+  // constants
+  val TRASH = "trash"
+  val CLASS_CAST_EXCEPTION_MESSAGE = "getException failed on Success object"
+
   // extensions
-  implicit def functions[T](src: Try[T]): Functions[T] = Functions(src)
+  implicit def functions[T](src: Try[T]): Core[T] = Core(src)
 
   implicit def errorHandling[T](src: Try[T]): ErrorHandling[T] = ErrorHandling(src)
 
   // attributes
-  private implicit val _errors: ListBuffer[Failure[Throwable]] = ListBuffer[Failure[Throwable]]()
+  private implicit val _errors: mutable.Map[String, ListBuffer[Failure[Throwable]]] = mutable.Map[String, ListBuffer[Failure[Throwable]]]()
 
   // functions
-  def getErrors: ListBuffer[Failure[Throwable]] = _errors
+  def getErrors(key: String): ListBuffer[Failure[Throwable]] = {
+    if (_errors.contains(key))
+      _errors(key)
+    else {
+      _errors.update(key, EMPTY)
+      EMPTY
+    }
+  }
 
-  def cleanErrors(): Unit = _errors.clear()
+  def EMPTY: ListBuffer[Failure[Throwable]] = ListBuffer[Failure[Throwable]]()
+
+  def getTrashErrors: ListBuffer[Failure[Throwable]] = {
+    if (_errors.contains(TRASH)) _errors(TRASH)
+    else {
+      _errors.update(TRASH, EMPTY)
+      EMPTY
+    }
+  }
+
+  def getAllErrors: Iterable[Failure[Throwable]] = _errors.values.flatten
+
+  def cleanErrors(key: String): Boolean = if (_errors.get(key).map(_.clear).isDefined) true else false
+
+  def cleanTrashErrors: Boolean = if (_errors.get(TRASH).map(_.clear).isDefined) true else false
+
+  def cleanAllErrors: Boolean = if (Option(_errors.map(_._2.clear())).isDefined) true else false
 }
 
-case class ErrorHandling[T](src: Try[T])(implicit _errors: ListBuffer[Failure[Throwable]]) {
+case class ErrorHandling[T](src: Try[T])(implicit _errors: mutable.Map[String, ListBuffer[Failure[Throwable]]]) {
+
+  def addError(key: String): Try[T] = {
+    src match {
+      case Success(_) => src
+      case Failure(e) =>
+        _errors.get(key) match {
+          case Some(value) => value.addOne(Failure(e))
+          case None => _errors.update(TRASH, ListBuffer(Failure(e)))
+        }
+        Failure(e)
+    }
+  }
 
   def addError(): Try[T] = {
     src match {
       case Success(_) => src
       case Failure(e) =>
-        _errors += Failure(e)
+        _errors.get(TRASH) match {
+          case Some(value) => value.addOne(Failure(e))
+          case None => _errors.update(TRASH, ListBuffer(Failure(e))) // update return Unit VS put return values of map
+        }
         Failure(e)
     }
   }
 }
 
-case class Functions[T](src: Try[T]) {
+case class ErrorsHandling[T](src: Iterable[Try[T]])(implicit _errors: mutable.Map[String, ListBuffer[Failure[Throwable]]]) {
+
+  def addErrors(key: String): Iterable[Try[T]] = src.map(s => ErrorHandling(s).addError(key)) // TODO: missing test
+
+  def addError(): Iterable[Try[T]] = src.map(s => ErrorHandling(s).addError()) // TODO: missing test
+
+  def getValidElements: Iterable[T] = src.filter(_.isSuccess).map(_.get) // TODO: missing test
+
+  def getInvalidElements: Iterable[Failure[T]] = src.filter(_.isFailure).asInstanceOf[Iterable[Failure[T]]] // TODO: missing test
+}
+
+case class Core[T](src: Try[T]) {
 
   def onFailure[U](func: Throwable => U): Try[T] = {
     src match {
@@ -48,7 +106,7 @@ case class Functions[T](src: Try[T]) {
 
   def getException: Try[Throwable] = {
     src match {
-      case Success(_) => Failure(new ClassCastException("getException failed on Success object"))
+      case Success(_) => Failure(new ClassCastException(CLASS_CAST_EXCEPTION_MESSAGE))
       case Failure(e) => Success(e)
     }
   }
--- src/test/scala/unit/futures/TryExtensionsTest.scala
+++ src/test/scala/unit/futures/TryExtensionsTest.scala
@@ -11,39 +11,37 @@ import scala.util.{Failure, Success}
 trait TryExtensionsTestConstants {
 
   // constants
-  val ErrorMessageI = "Intentionally caused error I"
-  val ErrorMessageII = "Intentionally caused error II"
-  val EnrichmentMessage = " - Enrichment Message"
-  val SuccessMessage = "Success Message"
-  val failureMessageI: Failure[Exception] = Failure(new Exception(ErrorMessageI))
-  val failureMessageIEnriched: Failure[Exception] = Failure(new Exception(ErrorMessageI + EnrichmentMessage))
-  val failureSuccessMessage: Failure[Exception] = Failure(new ClassCastException("getException failed on Success object"))
-  val successObject: Success[String] = Success(SuccessMessage)
-  val errors: ListBuffer[Failure[Throwable]] = TryExtensions.getErrors
-  val expectedErrors = List(failureSuccessMessage, failureMessageIEnriched)
+  val ERROR_MESSAGE_I = "Intentionally caused error I"
+  val ERROR_MESSAGE_II = "Intentionally caused error II"
+  val ENRICHMENT_MESSAGE = " - Enrichment Message"
+  val SUCCESS_MESSAGE = "Success Message"
+  val SUCCESS_MESSAGE_VALUE = "getException failed on Success object"
+  val failureMessageI: Failure[Exception] = Failure(new Exception(ERROR_MESSAGE_I))
+  val failureMessageIEnriched: Failure[Exception] = Failure(new Exception(ERROR_MESSAGE_I + ENRICHMENT_MESSAGE))
+  val failureSuccessMessage: Failure[Exception] = Failure(new ClassCastException(SUCCESS_MESSAGE_VALUE))
+  val successObject: Success[String] = Success(SUCCESS_MESSAGE)
+  val expectedErrors = List(failureSuccessMessage, failureMessageIEnriched, SUCCESS_MESSAGE_VALUE)
+
+  def myErrors: String => ListBuffer[Failure[Throwable]] = TryExtensions.getErrors
+
+  def trashErrors: ListBuffer[Failure[Throwable]] = TryExtensions.getTrashErrors
+
+  def allErrors: Iterable[Failure[Throwable]] = TryExtensions.getAllErrors
 
   // functions
   def printException: Throwable => Unit = { e => println(e.getMessage) }
 
-  def enrichmentException: Throwable => Throwable = { e => new Exception(e.getMessage + EnrichmentMessage) }
+  def enrichmentException: Throwable => Throwable = { e => new Exception(e.getMessage + ENRICHMENT_MESSAGE) }
 }
 
-class TryExtensionsTest extends AnyFlatSpec with TryExtensionsTestConstants with BeforeAndAfterAll {
-
-
-  // Init - Class
-  override protected def beforeAll: Unit = {
-    TryExtensions.cleanErrors()
-    assert(errors.isEmpty)
-    super.beforeAll()
-  }
+class TryExtensionsCoreTest extends AnyFlatSpec with TryExtensionsTestConstants with BeforeAndAfterAll {
 
   "A Failure Object" should "execute the onFailure function, show error message in console and return failure object" in {
 
     val result = failureMessageI.onFailure(printException)
 
     assert(result.isFailure)
-    assert(result.getOrElse(ErrorMessageII).toString === ErrorMessageII)
+    assert(result.getOrElse(ERROR_MESSAGE_II).toString === ERROR_MESSAGE_II)
   }
 
   it should "execute the onFailure function, enrichment exception and return failure object" in {
@@ -51,18 +49,7 @@ class TryExtensionsTest extends AnyFlatSpec with TryExtensionsTestConstants with
     val result = failureMessageI.onFailure(enrichmentException)
 
     assert(result.isFailure)
-    assert(result.getOrElse(ErrorMessageII).toString === ErrorMessageII)
-  }
-
-  it should "execute the onFailure function, enrichment exception, add error and return failure object" in {
-
-    val result = failureMessageI.onFailure(enrichmentException).addError()
-
-    assert(result.isFailure)
-    assert(result.getOrElse(ErrorMessageII).toString === ErrorMessageII)
-    assert(errors.nonEmpty)
-    // exists: method takes a predicate function and will use it to find the first element in the collection which matches the predicate.
-    assert(errors.exists(_.toString === failureMessageIEnriched.toString))
+    assert(result.getOrElse(ERROR_MESSAGE_II).toString === ERROR_MESSAGE_II)
   }
 
   it should "execute the onFailure function, enrichment exception, get exception and return exception" in {
@@ -80,10 +67,32 @@ class TryExtensionsTest extends AnyFlatSpec with TryExtensionsTestConstants with
     val result = successObject.onFailure(printException)
 
     assert(result.isSuccess)
-    assert(result.getOrElse(ErrorMessageI) === SuccessMessage)
+    assert(result.getOrElse(ERROR_MESSAGE_I) === SUCCESS_MESSAGE)
   }
 
-  it should "execute the onFailure function, not show error message in console, get exception, add error and return failure object" in {
+}
+
+class TryExtensionsErrorHandlingTest extends AnyFlatSpec with TryExtensionsTestConstants with BeforeAndAfterAll {
+
+  // Init - Class
+  override protected def beforeAll: Unit = {
+    super.beforeAll()
+    TryExtensions.cleanAllErrors
+    assert(trashErrors.isEmpty)
+  }
+
+  "A Failure Object" should "execute the onFailure function, enrichment exception, add error and return failure object" in {
+
+    val result = failureMessageI.addError().onFailure(enrichmentException).addError()
+
+    assert(result.isFailure)
+    assert(result.getOrElse(ERROR_MESSAGE_II).toString === ERROR_MESSAGE_II)
+    assert(trashErrors.nonEmpty)
+    // exists: method takes a predicate function and will use it to find the first element in the collection which matches the predicate.
+    assert(trashErrors.exists(_.toString === failureMessageIEnriched.toString))
+  }
+
+  "A Success Object" should "execute the onFailure function, not show error message in console, get exception, add error (general) and return failure object" in {
 
     def printException: Throwable => Unit = { e => println(e.getMessage) }
 
@@ -91,19 +100,47 @@ class TryExtensionsTest extends AnyFlatSpec with TryExtensionsTestConstants with
 
     assert(result.isFailure)
     result.onFailure(e => assert(e.getMessage === "getException failed on Success object"))
-    assert(errors.nonEmpty)
-    assert(errors.exists(_.toString contains "getException"))
+    assert(trashErrors.nonEmpty)
+    assert(trashErrors.exists(_.toString contains "getException"))
+  }
+
+  it should "execute the onFailure function, not show error message in console, get exception, add error (particular) and return failure object" in {
+
+    assert(myErrors(successObject.get).isEmpty)
+
+    def printException: Throwable => Unit = { e => println(e.getMessage) }
+
+    val result = successObject.onFailure(printException).getException.addError(successObject.get)
+
+    assert(result.isFailure)
+    result.onFailure(e => assert(e.getMessage === "getException failed on Success object"))
+    assert(myErrors(successObject.get).nonEmpty)
+    assert(myErrors(successObject.get).exists(_.toString contains "getException"))
   }
 
   // Finish - Class
   override protected def afterAll: Unit = {
-    assert(errors.nonEmpty)
-    assert(errors.size === 2)
-    // forall: returns true if all elements in the collection such that p(x) is true.
-    assert(expectedErrors.map(_.toString) forall errors.toString().contains)
-    TryExtensions.cleanErrors()
-    assert(errors.isEmpty)
     super.afterAll()
+    assert(validateTrashErrors)
+    assert(validateAllErrors)
+    TryExtensions.cleanAllErrors
+    assert(allErrors.isEmpty)
+  }
+
+  private def validateAllErrors: Boolean = {
+    // all errors
+    assert(allErrors.nonEmpty)
+    assert(allErrors.size === 4)
+    // forall: returns true if all elements in the collection such that p(x) is true.
+    expectedErrors.map(_.toString) forall allErrors.toString().contains
+  }
+
+  private def validateTrashErrors: Boolean = {
+    // trash errors
+    assert(trashErrors.nonEmpty)
+    assert(trashErrors.size === 3)
+    // forall: returns true if all elements in the collection such that p(x) is true.
+    expectedErrors.map(_.toString) forall trashErrors.toString().contains
   }
 
 }
